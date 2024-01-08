ba81f9f59ffaba24d81c5d475f4fc601553c24af
KDScheuer <KDScheuer97@gmail.com>
2024-01-07 22:28:01 -0700

    Fixed Formating and Spelling Errors

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 from button import Button
 
+
 def player_progress():
     progress = {}
     if os.path.exists('Blackout_Save_Data.txt'):
@@ -18,7 +19,7 @@ def player_progress():
 
 
 def block1(progress):
-    # Get Global Varibles to Establish Block 1 Game Loop
+    # Get Global Variables to Establish Block 1 Game Loop
     global screen, clock, running
 
     # Set Menu Caption on Window
@@ -39,13 +40,13 @@ def block1(progress):
 
 
 def menu(progress):
-    # Get Global Varibles to Establish Menu Game Loop
+    # Get Global Variables to Establish Menu Game Loop
     global screen, clock, running
 
     # Set Menu Caption on Window
     pygame.display.set_caption('BLACKOUT / Menu')
 
-    # Initalizing Menu Text and Varibables
+    # Initializing Menu Text and Variables
     title_font = pygame.font.Font(None, 100)
     button_font = pygame.font.Font(None, 50)
     title = "BLACKOUT"
@@ -59,19 +60,17 @@ def menu(progress):
     for value in progress.values():
         earned_stars += value
 
-    # Initalizing Players Earned Stars
+    # Initializing Players Earned Stars
     stars = str(earned_stars) + '/' + str(total_stars) + ' stars'
     star_font = pygame.font.Font(None, 75)
     star_surface = star_font.render(stars, True, star_desc_color)
 
-    # Initalizing Game Description
+    # Initializing Game Description
     description1 = '''After a city wide black-out, the city needs you to reconnect the'''
     description_font1 = pygame.font.Font(None, 40)
     description_surface1 = description_font1.render(description1, True, star_desc_color)
     description2 = '''buildings and restore power reconnect the buildings and restore power'''
-    description_font2 = pygame.font.Font(None, 40)
     description_surface2 = description_font1.render(description2, True, star_desc_color)
-
 
     # Menu Game Loop
     while running:
@@ -81,6 +80,22 @@ def menu(progress):
         # Getting Mouse Position
         mouse_pos = pygame.mouse.get_pos()
 
+        # Updating Level Buttons
+        block1_text = 'BLOCK 1' + '    Stars: ' + str(progress['Block 1'])
+        block1_surface = button_font.render(block1_text, True, title_color)
+        block1_button = Button(text=block1_text, pos=(790 - block1_surface.get_width() // 2, 350))
+        block2_text = 'BLOCK 2' + '    Stars: ' + str(progress['Block 2'])
+        block2_button = Button(text=block2_text, pos=(790 - block1_surface.get_width() // 2, 400))
+        block3_text = 'BLOCK 3' + '    Stars: ' + str(progress['Block 3'])
+        block3_button = Button(text=block3_text, pos=(790 - block1_surface.get_width() // 2, 450))
+        block4_text = 'BLOCK 4' + '    Stars: ' + str(progress['Block 4'])
+        block4_button = Button(text=block4_text, pos=(790 - block1_surface.get_width() // 2, 500))
+        block5_text = 'BLOCK 5' + '    Stars: ' + str(progress['Block 5'])
+        block5_button = Button(text=block5_text, pos=(790 - block1_surface.get_width() // 2, 550))
+        quit_text = 'QUIT'
+        quit_button = Button(text=quit_text, pos=(790 - block1_surface.get_width() // 2, 650))
+
+        # Checking Game Events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -92,32 +107,12 @@ def menu(progress):
                     block1(progress)
                     return
 
-        # Updating Level Buttons
-        block1_text = 'BLOCK 1' + '    Stars: ' + str(progress['Block 1'])
-        block1_surface = button_font.render(block1_text, True, title_color)
-        block1_button = Button(text=block1_text, pos=(790 - block1_surface.get_width() // 2, 350))
-        block2_text = 'BLOCK 2' + '    Stars: ' + str(progress['Block 2'])
-        block2_surface = button_font.render(block1_text, True, title_color)
-        block2_button = Button(text=block2_text, pos=(790 - block1_surface.get_width() // 2, 400))
-        block3_text = 'BLOCK 3' + '    Stars: ' + str(progress['Block 3'])
-        block3_surface = button_font.render(block1_text, True, title_color)
-        block3_button = Button(text=block3_text, pos=(790 - block1_surface.get_width() // 2, 450))
-        block4_text = 'BLOCK 4' + '    Stars: ' + str(progress['Block 4'])
-        block4_surface = button_font.render(block1_text, True, title_color)
-        block4_button = Button(text=block4_text, pos=(790 - block1_surface.get_width() // 2, 500))
-        block5_text = 'BLOCK 5' + '    Stars: ' + str(progress['Block 5'])
-        block5_surface = button_font.render(block1_text, True, title_color)
-        block5_button = Button(text=block5_text, pos=(790 - block1_surface.get_width() // 2, 550))
-        quit_text = 'QUIT'
-        quit_surface = button_font.render(block1_text, True, title_color)
-        quit_button = Button(text=quit_text, pos=(790 - block1_surface.get_width() // 2, 650))
-
-        #Writing Buttons to Screen
+        # Writing Buttons to Screen
         for button in [block1_button, block2_button, block3_button, block4_button, block5_button, quit_button]:
             button.changeColor(mouse_pos)
             button.update(screen)
 
-        #Writing Title, Stars, and Description to Screen
+        # Writing Title, Stars, and Description to Screen
         screen.blit(title_surface, (635 - title_surface.get_width() // 2, 50 - title_surface.get_height() // 2))
         screen.blit(star_surface, (635 - star_surface.get_width() // 2, 150 - title_surface.get_height() // 2))
         screen.blit(description_surface1, (350 - star_surface.get_width() // 2, 225 - title_surface.get_height() // 2))
@@ -127,11 +122,12 @@ def menu(progress):
         pygame.display.update()
         clock.tick(60)
 
+
 def main():
     # Pygame Initialization
     global screen, clock, running
     pygame.init()
-    screen = pygame.display.set_mode((1270,720))
+    screen = pygame.display.set_mode((1270, 720))
     clock = pygame.time.Clock()
     running = True
 
@@ -141,20 +137,7 @@ def main():
     # Display Main Menu
     menu(progress)
 
-    # Main Game Loop
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-
-        screen.fill("purple")
-
-        # Render to Screen
-        pygame.display.flip()
-
-        # Limit FPS
-        clock.tick(60)
-
+    # Exit Game
     pygame.quit()
 
 
